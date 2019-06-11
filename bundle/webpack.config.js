0b4e4260aaeb7fd80c9316576b093b783347e442
Artur Signell <artur@vaadin.com>
2019-06-11 14:01:26 +0300

    Create webapp folder if not present

--- bundle/webpack.config.js
+++ bundle/webpack.config.js
@@ -97,6 +97,7 @@ module.exports = {
       compiler.hooks.afterEmit.tapAsync("FlowIdPlugin", (compilation, done) => {
         console.log("Emitted " + statsFile)
         const bundleFilename = compilation.getStats().toJson().assetsByChunkName.bundle.replace(/.*\//,"");
+        fs.mkdirSync("src/main/webapp");
         fs.copyFile('index.nocache.js', indexNoCacheFile, (err) => {
           if (err) throw err;
         });
